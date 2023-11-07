531505253c6b5acd96b2af489657fe339425eaba
nongnong10 <jirachi@gmail.com>
2023-11-07 17:17:14 +0700

    [admin] basic setup, add theme

--- P4L/src/App.jsx
+++ P4L/src/App.jsx
@@ -2,16 +2,26 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { ColorModeContext, useMode } from './theme';
+import { CssBaseline, ThemeProvider } from '@mui/material';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [theme, colorMode] = useMode();
 
   return (
-    <>
-      <div>
-        Hello
-      </div>
-    </>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className='app'>
+          <main className='content'>
+            <h1>
+              Hello
+            </h1>
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   )
 }
 
