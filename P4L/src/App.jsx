ca6deb5eba1463b1ff2b252702dac8785ae289fd
nongnong10 <jirachi@gmail.com>
2023-11-08 18:31:04 +0700

    [admin] add Manage Team and update name

--- P4L/src/App.jsx
+++ P4L/src/App.jsx
@@ -8,6 +8,7 @@ import { Routes, Route } from 'react-router-dom';
 import Topbar from './scenes/global/Topbar';
 import Sidebar from './scenes/global/Sidebar';
 import Dashboard from './scenes/dashboard';
+import Team from './scenes/team';
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -23,6 +24,7 @@ function App() {
             <Topbar setIsSidebar={setIsSidebar} />
             <Routes>
               <Route path="/" element={<Dashboard />} />
+              <Route path="/team" element={<Team />} />
             </Routes>
           </main>
         </div>
--- P4L/src/scenes/global/Sidebar.jsx
+++ P4L/src/scenes/global/Sidebar.jsx
@@ -75,7 +75,7 @@ const Sidebar = () => {
                 ml="15px"
               >
                 <Typography variant="h3" color={colors.grey[100]}>
-                  ADMINIS
+                  Plant4Life
                 </Typography>
                 <IconButton onClick={() => setIsCollapsed(!isCollapsed)}>
                   <MenuOutlinedIcon />
