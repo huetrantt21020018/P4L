7fe40629d823e6f4d195d297397f1f90670bc2ff
nongnong10 <jirachi@gmail.com>
2023-11-08 12:24:37 +0700

    [admin] add Sidebar

--- P4L/src/App.jsx
+++ P4L/src/App.jsx
@@ -4,24 +4,31 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import { ColorModeContext, useMode } from './theme';
 import { CssBaseline, ThemeProvider } from '@mui/material';
+import { Routes, Route } from 'react-router-dom';
 import Topbar from './scenes/global/Topbar';
+import Sidebar from './scenes/global/Sidebar';
+import Dashboard from './scenes/dashboard';
 
 function App() {
-  const [count, setCount] = useState(0);
   const [theme, colorMode] = useMode();
+  const [isSidebar, setIsSidebar] = useState(true);
 
   return (
     <ColorModeContext.Provider value={colorMode}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-        <div className='app'>
-          <main className='content'>
-            <Topbar />
+        <div className="app">
+          <Sidebar isSidebar={isSidebar} />
+          <main className="content">
+            <Topbar setIsSidebar={setIsSidebar} />
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+            </Routes>
           </main>
         </div>
       </ThemeProvider>
     </ColorModeContext.Provider>
-  )
+  );
 }
 
 export default App
